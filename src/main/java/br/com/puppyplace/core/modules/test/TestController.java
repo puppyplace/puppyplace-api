83ef3f4f027635fc6362e4c6f81482e75146b85d
Petrus de Melo <petrusdemelo@gmail.com>
2021-03-27 10:37:05 -0300

    Adding swagger to dependencies

--- src/main/java/br/com/puppyplace/core/modules/test/TestController.java
+++ src/main/java/br/com/puppyplace/core/modules/test/TestController.java
@@ -1,6 +1,5 @@
 package br.com.puppyplace.core.modules.test;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
