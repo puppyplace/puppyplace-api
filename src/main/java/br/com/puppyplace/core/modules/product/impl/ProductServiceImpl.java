a284edf939eda254ccf66177cfdef70f75f8bf71
Petrus de Melo <petrusdemelo@gmail.com>
2021-04-04 17:48:20 -0300

    Creating first structure of product's module

---
+++ src/main/java/br/com/puppyplace/core/modules/product/impl/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package br.com.puppyplace.core.modules.product.impl;
+
+import br.com.puppyplace.core.modules.product.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+
+}
