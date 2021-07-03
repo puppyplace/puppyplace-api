ecf99c26d106bdb95256ac9d6052b85bcdeca61c
Beatriz Uezu <beatriz.uezu@gmail.com>
2021-07-03 00:54:57 -0300

    fix import

--- src/main/java/br/com/puppyplace/core/modules/address/AddressRepository.java
+++ src/main/java/br/com/puppyplace/core/modules/address/AddressRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AddressRepository extends CrudRepository<Address, UUID> {
